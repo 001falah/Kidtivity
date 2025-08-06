fc5774679e3a63740da3c95e182114e7651ccec7
001falah <falah07mohammed@gamil.com>
2025-08-06 13:19:23 +0530

    c

--- shop.js
+++ shop.js
@@ -28,3 +28,12 @@
     });
   }
 });
+
+ document.addEventListener('DOMContentLoaded', function () {
+  const buyButton = document.getElementById('buyBtn1');
+  if (buyButton) {
+    buyButton.addEventListener('click', function () {
+      window.location.href = 'checkout2.html';
+    });
+  }
+});
